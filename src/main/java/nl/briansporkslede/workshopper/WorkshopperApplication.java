1d4ed5aa841ee3432a10afaa786bfdbe513ecc1d
sprksld <b.sporkslede@xs4all.nl>
2023-02-02 13:13:13 +0100

    added spring boot

---
+++ src/main/java/nl/briansporkslede/workshopper/WorkshopperApplication.java
@@ -0,0 +1,13 @@
+package nl.briansporkslede.workshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/briansporkslede/workshopper/WorkshopperApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.briansporkslede.workshopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
